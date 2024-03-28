6e266a6e6f5a4c17b54e0e7b1deceb51ae2b3ed2
Bluce <a1019521a@gmail.com>
2024-03-28 10:34:15 +0800

    test>

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Router, Route } from "@solidjs/router";
 import { lazy } from "solid-js";
 const RouteGuard = lazy(() => import("./router/RouteGuard"));
-const Home = lazy(() => import("./pages/home"));
-//import Home from "./pages/home";
+//const Home = lazy(() => import("./pages/home"));
+import Home from "./pages/home";
 const Login = lazy(() => import("./pages/login"));
 
 function App() {

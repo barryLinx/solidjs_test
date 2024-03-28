5425e41040fabb0ae0861b8f4862de150657a179
Bluce <a1019521a@gmail.com>
2024-03-28 10:01:20 +0800

    test>

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,11 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import { lazy } from "solid-js";
-import { Router, Route } from "@solidjs/router";
+// import { lazy } from "solid-js";
+// import { Router, Route } from "@solidjs/router";
 import App from "./App";
+import "@/scss/bootstrap.scss";
+// Import all of Bootstrap's JS
+import "@/helper/bootstrap.js";
 const root = document.getElementById("root");
 
 render(
